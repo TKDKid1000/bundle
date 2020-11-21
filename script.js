877c49747e4567b30874d83040064e1c4a9e1f0e
TKDKid1000 <69736165+TKDKid1000@users.noreply.github.com>
2020-11-21 11:19:06 -0800

    Added a allow white space area

--- script.js
+++ script.js
@@ -66,3 +66,41 @@ function generatecmd() {
     }
     document.getElementById("outputcmd").innerHTML = `/give @s bundle{Items:[${nbt}]}`;
 }
+
+function generatecmdair() {
+    nbt = ""
+    var s1 = document.getElementById("s1").value;
+    nbt = nbt + `{Count:1b,id:"${s1}"},`
+    var s2 = document.getElementById("s2").value;
+    nbt = nbt + `{Count:1b,id:"${s2}"},`
+    var s3 = document.getElementById("s3").value;
+    nbt = nbt + `{Count:1b,id:"${s3}"},`
+    var s4 = document.getElementById("s4").value;
+    nbt = nbt + `{Count:1b,id:"${s4}"},`
+    var s5 = document.getElementById("s5").value;
+    nbt = nbt + `{Count:1b,id:"${s5}"},`
+    var s6 = document.getElementById("s6").value;
+    nbt = nbt + `{Count:1b,id:"${s6}"},`
+    var s7 = document.getElementById("s7").value;
+    nbt = nbt + `{Count:1b,id:"${s7}"},`
+    var s8 = document.getElementById("s8").value;
+    nbt = nbt + `{Count:1b,id:"${s8}"},`
+    var s9 = document.getElementById("s9").value;
+    nbt = nbt + `{Count:1b,id:"${s9}"},`
+    var s10 = document.getElementById("s10").value;
+    nbt = nbt + `{Count:1b,id:"${s10}"},`
+    var s11 = document.getElementById("s11").value;
+    nbt = nbt + `{Count:1b,id:"${s11}"},`
+    var s12 = document.getElementById("s12").value;
+    nbt = nbt + `{Count:1b,id:"${s12}"},`
+    var s13 = document.getElementById("s13").value;
+    nbt = nbt + `{Count:1b,id:"${s13}"},`
+    var s14 = document.getElementById("s14").value;
+    nbt = nbt + `{Count:1b,id:"${s14}"},`
+    var s15 = document.getElementById("s15").value;
+    nbt = nbt + `{Count:1b,id:"${s15}"},`
+    var s16 = document.getElementById("s16").value;
+    nbt = nbt + `{Count:1b,id:"${s16}"},`
+    document.getElementById("outputcmd").innerHTML = `/give @s bundle{Items:[${nbt}]}`;
+    
+}
